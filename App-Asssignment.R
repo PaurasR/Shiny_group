9eeadbbec48f9277f12745d06435c37320157d0b
Anitta Joseph <anittarj2219@gmail.com>
2024-12-21 19:40:57 +0000

    UI-Header

--- App-Asssignment.R
+++ App-Asssignment.R
@@ -6,5 +6,10 @@ library(ggplot2)
 library(DT)
 library(waiter)
 
-DIG <- read.csv("C:/Users/anitt/Downloads/DIG.csv")
+DIG <- read.csv("DIG.csv")
 DIG <- DIG %>% filter(!is.na(AGE) & !is.na(DEATH))
+
+# Convert categorical variables to factors
+DIG$TRTMT <- factor(DIG$TRTMT, levels = c(0, 1), labels = c("Placebo", "Treatment"))
+DIG$SEX <- factor(DIG$SEX, levels = c(1, 2), labels = c("Male", "Female"))
+DIG$RACE <- factor(DIG$RACE, levels = c(1, 2), labels = c("White", "Nonwhite"))

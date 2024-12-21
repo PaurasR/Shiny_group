825376e4837135bd01573fae3218cb71048f86bf
Anitta Joseph <anittarj2219@gmail.com>
2024-12-21 19:49:12 +0000

    UI-Dashboard-Header

--- App-Asssignment.R
+++ App-Asssignment.R
@@ -13,3 +13,35 @@ DIG <- DIG %>% filter(!is.na(AGE) & !is.na(DEATH))
 DIG$TRTMT <- factor(DIG$TRTMT, levels = c(0, 1), labels = c("Placebo", "Treatment"))
 DIG$SEX <- factor(DIG$SEX, levels = c(1, 2), labels = c("Male", "Female"))
 DIG$RACE <- factor(DIG$RACE, levels = c(1, 2), labels = c("White", "Nonwhite"))
+
+ui <- dashboardPage(
+  dashboardHeader(title = "DIG Trial Dashboard"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Overview", tabName = "overview", icon = icon("home")),
+      menuItem("Demographics", tabName = "demographics", icon = icon("users")),
+      menuItem("Clinical Measures", tabName = "clinical", icon = icon("stethoscope")),
+      menuItem("Outcomes", tabName = "outcomes", icon = icon("bar-chart"))
+    )
+  ),
+)
+dashboardBody(
+  use_waiter(),  # Using waiter package to show a loading animation
+  tabItems(
+    # Overview Tab
+    tabItem(
+      tabName = "overview",
+      fluidRow(
+        box(
+          title = "Dataset Overview", 
+          width = 12, 
+          status = "primary", 
+          solidHeader = TRUE,
+          collapsible = TRUE,
+          dataTableOutput("overviewTable")
+        )
+      )
+    ),
+    
+  )))
+
